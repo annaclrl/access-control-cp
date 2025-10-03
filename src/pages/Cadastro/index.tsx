1b2c31dcf810917658037a50d49b360e0e6ef259
Duartegdm <gduarte.study@gmail.com>
2025-10-03 16:21:18 -0300

    feat: adicionando campos no formulário para cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,8 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 const Cadastro = () =>{
+
+    const navigate = useNavigate();
     return (
         <main>
             <form>
                 <h1>Cadastro</h1>
+                <div>
+                    <label>Nome</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Nome de usuário</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email"/>
+                </div>
+                <button type="submit" onClick={() => navigate("/login")}> Cadastrar</button>
             </form>
         </main>
     )
