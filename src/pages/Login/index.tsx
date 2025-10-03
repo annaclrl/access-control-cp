c90c34a8488b08f45e5637b5e93b7de943209e19
Anna <annaclararussoluca@gmail.com>
2025-10-03 14:33:52 -0300

    feat: adicionando formulário para login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return(
-        <>
-        </>
+        <main>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label>Nome de usuário</label>
+                    <input type="text" name="nomeUsuario"/>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email"/>
+                </div>
+                <p> Ainda não tem uma conta? <Link to={"/cadastro"}>Cadastre-se aqui</Link></p>
+            </form>
+        </main>
     )
 }
 
