50d670b675e3677761a3d117f81ad9e16812cc5f
Duartegdm <gduarte.study@gmail.com>
2025-10-03 20:08:55 -0300

    style: estilizando página de cadastro com Tailwind

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -41,11 +41,11 @@ const Cadastro = () => {
     });
 
      return (
-        <main>
-            <form onSubmit={onSubmit}>
-                <h1>Cadastro</h1>
-                <div>
-                    <label>Nome</label>
+        <main className="flex justify-center items-center h-screen bg-gray-500">
+            <form onSubmit={onSubmit} className="bg-white p-6 rounded-2xl shadow-md w-90">
+                <h1 className="text-xl font-bold mb-4 text-center">Cadastro</h1>
+                <div className="mb-4">
+                    <label className="block mb-2">Nome</label>
                     <input type="text"
                         {...register("nome",
                             {
@@ -53,11 +53,12 @@ const Cadastro = () => {
                                 minLength: { value: 3, message: "O nome deve ter no mínimo 3 caracteres" },
                             })
                         }
+                        className="w-full border rounded p-2"
                     />
-                    {errors.nome && <p>{errors.nome.message}</p>}
+                    {errors.nome && <p className="text-red-500 text-sm mt-1">{errors.nome.message}</p>}
                 </div>
-                <div>
-                    <label>Nome de usuário</label>
+                <div className="mb-4">
+                    <label className="block mb-2">Nome de usuário</label>
                     <input type="text"
                         {...register("nomeUsuario",
                             {
@@ -65,11 +66,12 @@ const Cadastro = () => {
                                 minLength: { value: 3, message: "O nome de usuário deve ter no mínimo 3 caracteres" },
                             })
                         }
+                        className="w-full border rounded p-2"
                     />
-                    {errors.nomeUsuario && <p>{errors.nomeUsuario.message}</p>}
+                    {errors.nomeUsuario && <p className="text-red-500 text-sm mt-1">{errors.nomeUsuario.message}</p>}
                 </div>
-                <div>
-                    <label>Email</label>
+                <div className="mb-4">
+                    <label className="block mb-2">Email</label>
                     <input type="email"
                         {...register("email",
                             {
@@ -77,10 +79,11 @@ const Cadastro = () => {
                                 minLength: { value: 10, message: "O email deve ter no mínimo 10 caracteres" },
                             })
                         }
+                        className="w-full border rounded p-2"
                     />
-                    {errors.email && <p>{errors.email.message}</p>}
+                    {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
                 </div>
-                <button type="submit" onClick={() => navigate("/login")}>Cadastrar</button>
+                <button type="submit" className="w-full bg-gray-800 text-white rounded p-2 ">Cadastrar</button>
             </form>
         </main>
     )
