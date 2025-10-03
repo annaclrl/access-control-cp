9ddd550a2161326881e11b86df45f45c4506a436
Duartegdm <gduarte.study@gmail.com>
2025-10-03 16:13:54 -0300

    feat: configurando rota /cadastro

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login"
+import Cadastro from "../pages/Cadastro";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <Login/>,
+    },
+    {
+        path: "/cadastro",
+        element: <Cadastro/>,
     }
 ])
 
